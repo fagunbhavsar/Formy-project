1470012d29a8d629966358771d44ca3557758ef9
Fagun Bhavsar <fagun.bhavsar@gmail.com>
2019-05-24 16:28:11 +0530

    Web form scenario testing

---
+++ features/steps/webform.rb
@@ -0,0 +1,43 @@
+Given(/^I formy project app link$/) do
+  pending
+end
+
+Then(/^I click on Complete web form link$/) do
+  pending
+end
+
+Then(/^I enter first name as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I enter last name as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I enter job title as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I select an option as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I select a checkbox as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I select years of experience as "10\+"$/) do |arg|
+  pending
+end
+
+Then(/^I select date as "05\/24\/2019"$/) do |arg|
+  pending
+end
+
+And(/^I click on Submit button$/) do
+  pending
+end
+
+Then(/^I see success message as "([^"]*)"$/) do |arg|
+  pending
+end
---
+++ features/webform.feature
@@ -0,0 +1,20 @@
+
+@webform @functional
+
+  Feature: Fill the web form with given details
+
+  @formdetails
+  Scenario: Fill the web form with given details
+    Given I formy project app link
+    Then I click on Complete web form link
+    Then I enter first name as "Fagun"
+    Then I enter last name as "Bhavsar"
+    Then I enter job title as "QA analyst"
+    Then I select an option as "College"
+    Then I select a checkbox as "Male"
+    Then I select years of experience as "10+"
+    Then I select date as "05/24/2019"
+    And  I click on Submit button
+    Then I see success message as "The form was successfully submitted!"
+
+
