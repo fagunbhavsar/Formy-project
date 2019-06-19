977bebe2987bc05347a932020e1f39271532c617
Fagun Bhavsar <fagun.bhavsar@gmail.com>
2019-06-19 17:06:14 +0530

    Webform steps were modified

--- features/steps/webform.rb
+++ features/steps/webform.rb
@@ -18,19 +18,19 @@ Then("I enter job title as {string}") do |string|
   on(CompleteForm).enter_job_title(string)
 end
 
-Then("I select an option as {string}") do
+Then("I select an option") do
   on(CompleteForm).select_college
 end
 
-Then("I select a checkbox as {string}") do
+Then("I select a checkbox") do
   on(CompleteForm).check_gender
 end
 
-Then("I select years of experience as {string}") do
+Then("I select years of experience") do
   on(CompleteForm).select_exp
 end
 
-Then("I select date as {string}") do
+Then("I select date") do
   on(CompleteForm).select_date
 end
 
@@ -38,7 +38,7 @@ And("I click on Submit button") do
   on(CompleteForm).click_submit
 end
 
-Then("I see success message as {string}") do
+Then("I see success message") do
   v = on(CompleteForm).view_confirmation
   expect(v).to eq(true)
 end
